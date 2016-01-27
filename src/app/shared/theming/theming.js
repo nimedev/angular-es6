541f7b6b26ffe14170c5f517adfc31587517f4f4
nimedev <nicolasmejiaco@gmail.com>
2016-01-27 13:55:24 -0500

    jspm: improve for bundle

--- src/app/shared/theming/theming.js
+++ src/app/shared/theming/theming.js
@@ -7,7 +7,6 @@ import * as angular from 'angular';
 
 /** Comunity modules */
 import ngMaterial from 'angular-material';
-import 'jspm_packages/npm/angular-material@1.0.2/angular-material.min.css!';
 
 // Constants
 const moduleName = 'theming';
