748b91bad0f38c472084b90cbe2a9790e28d0336
nimedev <nicolasmejiaco@gmail.com>
2016-03-20 10:08:36 -0500

    fix angular-translate update dependencie

--- src/config.js
+++ src/config.js
@@ -17,11 +17,11 @@ System.config({
     "angular-animate": "npm:angular-animate@1.5.2",
     "angular-aria": "npm:angular-aria@1.5.2",
     "angular-cookies": "npm:angular-cookies@1.5.2",
-    "angular-translate": "npm:angular-translate@2.9.1",
-    "angular-translate-handler-log": "npm:angular-translate-handler-log@2.9.1",
-    "angular-translate-loader-static-files": "npm:angular-translate-loader-static-files@2.9.1",
-    "angular-translate-storage-cookie": "npm:angular-translate-storage-cookie@2.9.1",
-    "angular-translate-storage-local": "npm:angular-translate-storage-local@2.9.1",
+    "angular-translate": "github:angular-translate/bower-angular-translate@2.10.0",
+    "angular-translate-handler-log": "github:angular-translate/bower-angular-translate-handler-log@2.10.0",
+    "angular-translate-loader-static-files": "github:angular-translate/bower-angular-translate-loader-static-files@2.10.0",
+    "angular-translate-storage-cookie": "github:angular-translate/bower-angular-translate-storage-cookie@2.10.0",
+    "angular-translate-storage-local": "github:angular-translate/bower-angular-translate-storage-local@2.10.0",
     "angular-ui-router": "npm:angular-ui-router@0.2.18",
     "babel": "npm:babel-core@5.8.35",
     "babel-runtime": "npm:babel-runtime@5.8.35",
@@ -30,6 +30,18 @@ System.config({
     "css": "github:systemjs/plugin-css@0.1.20",
     "json": "github:systemjs/plugin-json@0.1.0",
     "text": "github:systemjs/plugin-text@0.0.4",
+    "github:angular-translate/bower-angular-translate-handler-log@2.10.0": {
+      "angular-translate": "github:angular-translate/bower-angular-translate@2.10.0"
+    },
+    "github:angular-translate/bower-angular-translate-loader-static-files@2.10.0": {
+      "angular-translate": "github:angular-translate/bower-angular-translate@2.10.0"
+    },
+    "github:angular-translate/bower-angular-translate-storage-cookie@2.10.0": {
+      "angular-translate": "github:angular-translate/bower-angular-translate@2.10.0"
+    },
+    "github:angular-translate/bower-angular-translate-storage-local@2.10.0": {
+      "angular-translate": "github:angular-translate/bower-angular-translate@2.10.0"
+    },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -75,22 +87,6 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:angular-animate@1.5.2": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:angular-translate-handler-log@2.9.1": {
-      "angular-translate": "npm:angular-translate@2.9.1"
-    },
-    "npm:angular-translate-loader-static-files@2.9.1": {
-      "angular-translate": "npm:angular-translate@2.9.1"
-    },
-    "npm:angular-translate-storage-cookie@2.9.1": {
-      "angular-translate": "npm:angular-translate@2.9.1"
-    },
-    "npm:angular-translate-storage-local@2.9.1": {
-      "angular-translate": "npm:angular-translate@2.9.1",
-      "angular-translate-storage-cookie": "npm:angular-translate-storage-cookie@2.9.1"
-    },
-    "npm:angular-translate@2.9.1": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:angular-ui-router@0.2.18": {
