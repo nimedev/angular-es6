a6896b949c3fe3354fc45154b4e8413b17f51619
nimedev <nicolasmejiaco@gmail.com>
2016-03-27 19:27:39 -0500

    update nmd-dialog component.
    Detect event to close dialog

--- src/app/components/nmd-components/nmd-dialog/dialog-watcher.directive.js
+++ src/app/components/nmd-components/nmd-dialog/dialog-watcher.directive.js
@@ -3,13 +3,14 @@
  * @name dialogWatcher
  * @param {Object} $compile - to compile dialog content component.
  * @param {Object} $timeout - to generate delay for destroy element from DOM
+ * @param {Object} $window - to listen cancel key.
  * @param {Object} nmdDialog - get close service.
  */
 // Directive name
 const directiveName = 'dialogWatcher'
 
 // Directive Function
-const directive = ($compile, $timeout, nmdDialog) => {
+const directive = ($compile, $timeout, $window, nmdDialog) => {
   let directive = {
     link: link,
     restrict: 'A'
@@ -21,7 +22,7 @@ const directive = ($compile, $timeout, nmdDialog) => {
   function link(scope, element, attrs) {
     let backdrop = element[0].querySelector('.dialog__back')
     let component = attrs.ndComponent
-      
+
     // Insert component to dialog content
     if (component) {
       let newElement = `<${component} class="dialog__component whiteframe-5"></${component}>`
@@ -30,22 +31,57 @@ const directive = ($compile, $timeout, nmdDialog) => {
 
     // Add click event to backdrop element to close dialog
     backdrop.addEventListener('click', () => nmdDialog.close(component))
-      
+
+    // Add 'keydown' event to listen cancel key
+    $window.addEventListener('keydown', checkKeyPressed)
+
+    // Add 'hashchange' event to close the dialog if url change
+    $window.addEventListener('hashchange', hashChanged)
+
     // Wait for the scope destruction 
     scope.$on('$destroy', () => {
       // remove component from DOM.
-      // Insert a delay to elminate for CSS animation porpouses.
       element.addClass('dialog--close')
+
+      // Remove events
+      $window.removeEventListener('keydown', checkKeyPressed)
+      $window.removeEventListener('hashchange', hashChanged)
+
+      // Insert a delay to elminate for CSS animation porpouses.
       $timeout(() => {
         element.remove()
         nmdDialog.toggleElements()
-      }, 250)
+      }, 1000)
     })
+
+    /** FUNCTIONS FOR EVENTS */
+    /**
+     * Check key pressed. Put inside link function for scope issues
+     * @memberOf link
+     * @param {Object} event - event for 'keydown' event
+     */
+    function checkKeyPressed(event) {
+      const keyCode = event.keyCode
+
+      // If key pressed is cancel (27). close de dialog
+      if (keyCode === 27) {
+        event.preventDefault()
+        nmdDialog.close(component)
+      }
+    }
+
+    /**
+     * Handle 'hashchange' event
+     * @memberOf link
+     */
+    function hashChanged() {
+      nmdDialog.close(component)
+    }
   }
 }
 
 // Injection array for minification compatibility
-directive.$inject = ['$compile', '$timeout', 'nmdDialog']
+directive.$inject = ['$compile', '$timeout', '$window', 'nmdDialog']
 
 /** @exports directive name and class */
 export default {
